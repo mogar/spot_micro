90bcd95ae0646ef89f726fab8af510247cb2b8c5
morgan <redfieldm@gmail.com>
2023-09-10 02:09:31 +0100

    turn servo_ctl into ROS node

--- spot_ws/src/joint_ctl/joint_ctl/servo_ctl.py
+++ spot_ws/src/joint_ctl/joint_ctl/servo_ctl.py
@@ -1,12 +1,30 @@
 
+import rclpy
+from rclpy.node import Node
 from joint_ctl.lib.servo import Servo, PcaPwm
 
-def main():
-    controller = PcaPwm(channel = 20)
-    # TODO: max, min, default?
-    flc_servo = Servo(controller, 0) # front-left coxa
+class SpotJoints(Node):
 
+    def __init__(self):
+        super().__init__("node_test")
+
+        self._controller = PcaPwm(channel = 20)
+        # TODO: max, min, default?
+        self._flc_servo = Servo(self._controller, 0) # front-left coxa
+        # TODO: other joints
+
+        self.counter_ = 0
+        self.get_logger().info("SpotJoints initialized")
+        # TODO: callback for messages
+
+
+def main(args=None):
     print('Hi from joint_ctl.')
+
+    rclpy.init(args=args)
+    node = SpotJoints()
+    rclpy.spin(node)
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
