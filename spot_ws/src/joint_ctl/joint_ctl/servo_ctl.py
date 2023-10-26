9a6e44ccb274ac0ea1963423fb7499fda1ebaaec
morgan <redfieldm@gmail.com>
2023-10-25 20:48:09 -0400

    parametrize angle limits on joint_ctl

--- spot_ws/src/joint_ctl/joint_ctl/servo_ctl.py
+++ spot_ws/src/joint_ctl/joint_ctl/servo_ctl.py
@@ -20,22 +20,42 @@ class SpotJoints(Node):
         
         # TODO: parametrize servo pin IDs?
 
+        # joint rotation limits
+        coxa_min = -30.0
+        coxa_max = 30.0
+        hip_min = -78.0
+        hip_max = 78.0
+        knee_min = -60.0
+        knee_max = 150.0
+
         # Front Left Leg
-        self._flc_servo = Servo(self._controller, 3, min_out = [rad(-30.0), 350], max_out = [rad(30.0), 450]) # front-left coxa
-        self._flh_servo = Servo(self._controller, 4, min_out = [rad(-78.0), 530], max_out = [rad(78.0), 270]) # front-left hip
-        self._flk_servo = Servo(self._controller, 5, min_out = [rad(-60.0), 460], max_out = [rad(90.0), 110]) # front-left knee
+        (flc_min, flc_max) = self.get_joint_limits(400, coxa_min, coxa_max)
+        self._flc_servo = Servo(self._controller, 3, min_out = flc_min, max_out = flc_max) # front-left coxa
+        (flh_min, flh_max) = self.get_joint_limits(400, hip_min, hip_max, True)
+        self._flh_servo = Servo(self._controller, 4, min_out = flh_min, max_out = flh_max) # front-left hip
+        (flk_min, flk_max) = self.get_joint_limits(360, knee_min, knee_max, True)
+        self._flk_servo = Servo(self._controller, 5, min_out = flk_min, max_out = flk_max) # front-left knee
         # Front Right Leg
-        self._frc_servo = Servo(self._controller, 0, min_out = [rad(-30.0), 330], max_out = [rad(30.0), 450]) # front-right coxa
-        self._frh_servo = Servo(self._controller, 1, min_out = [rad(-78.0), 220], max_out = [rad(78.0), 480]) # front-right hip
-        self._frk_servo = Servo(self._controller, 2, min_out = [rad(-60.0), 300], max_out = [rad(90.0), 650]) # front-right knee
+        (frc_min, frc_max) = self.get_joint_limits(380, coxa_min, coxa_max)
+        self._frc_servo = Servo(self._controller, 0, min_out = frc_min, max_out = frc_max) # front-right coxa
+        (frh_min, frh_max) = self.get_joint_limits(350, hip_min, hip_max)
+        self._frh_servo = Servo(self._controller, 1, min_out = frh_min, max_out = frh_max) # front-right hip
+        (frk_min, frk_max) = self.get_joint_limits(400, knee_min, knee_max)
+        self._frk_servo = Servo(self._controller, 2, min_out = frk_min, max_out = frk_max) # front-right knee
         # Back Left Leg
-        self._blc_servo = Servo(self._controller, 9, min_out = [rad(-30.0), 315], max_out = [rad(30.0), 415]) # back-left coxa
-        self._blh_servo = Servo(self._controller, 10, min_out = [rad(-78.0), 550], max_out = [rad(78.0), 290]) # back-left hip
-        self._blk_servo = Servo(self._controller, 11, min_out = [rad(-60.0), 470], max_out = [rad(90.0), 120]) # back-left knee
+        (blc_min, blc_max) = self.get_joint_limits(365, coxa_min, coxa_max)
+        self._blc_servo = Servo(self._controller, 9, min_out = blc_min, max_out = blc_max) # back-left coxa
+        (blh_min, blh_max) = self.get_joint_limits(420, hip_min, hip_max, True)
+        self._blh_servo = Servo(self._controller, 10, min_out = blh_min, max_out = blh_max) # back-left hip
+        (blk_min, blk_max) = self.get_joint_limits(370, knee_min, knee_max, True)
+        self._blk_servo = Servo(self._controller, 11, min_out = blk_min, max_out = blk_max) # back-left knee
         # Back Right Leg
-        self._brc_servo = Servo(self._controller, 6, min_out = [rad(-30.0), 320], max_out = [rad(30.0), 420]) # back-right coxa
-        self._brh_servo = Servo(self._controller, 7, min_out = [rad(-78.0), 210], max_out = [rad(78.0), 470]) # back-right hip
-        self._brk_servo = Servo(self._controller, 8, min_out = [rad(-60.0), 290], max_out = [rad(90.0), 640]) # back-right knee
+        (brc_min, brc_max) = self.get_joint_limits(370, coxa_min, coxa_max)
+        self._brc_servo = Servo(self._controller, 6, min_out = brc_min, max_out = brc_max) # back-right coxa
+        (brh_min, brh_max) = self.get_joint_limits(340, hip_min, hip_max)
+        self._brh_servo = Servo(self._controller, 7, min_out = brh_min, max_out = brh_max) # back-right hip
+        (brk_min, brk_max) = self.get_joint_limits(390, knee_min, knee_max)
+        self._brk_servo = Servo(self._controller, 8, min_out = brk_min, max_out = brk_max) # back-right knee
 
         self._target_joint_sub = self.create_subscription(
             JointAngles,
@@ -77,6 +97,17 @@ class SpotJoints(Node):
         current_joint_angles.brk = self._brk_servo.set_angle_rad(msg.brk)
 
         self._current_joint_pub.publish(current_joint_angles)
+
+    def get_joint_limits(self, zero_count: int, neg_limit_deg: float, pos_limit_deg: float, invert: bool = False):
+        # TODO: get degrees_per_count from a servo object or something?
+        degrees_per_count = 0.6
+        if not invert:
+            return ([rad(neg_limit_deg), int(zero_count + neg_limit_deg/degrees_per_count)],
+                    [rad(pos_limit_deg), int(zero_count + pos_limit_deg/degrees_per_count)])
+        else:
+            return ([rad(neg_limit_deg), int(zero_count - neg_limit_deg/degrees_per_count)],
+                    [rad(pos_limit_deg), int(zero_count - pos_limit_deg/degrees_per_count)])
+
 
 def main(args=None) -> None:
     rclpy.init(args=args)
