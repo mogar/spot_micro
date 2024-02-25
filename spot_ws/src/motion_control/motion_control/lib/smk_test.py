08a32aafc671fd3c40d9fffe3b420079c5d73496
morgan <redfieldm@gmail.com>
2024-02-25 01:23:02 +0000

    smk test

---
+++ spot_ws/src/motion_control/motion_control/lib/smk_test.py
@@ -0,0 +1,7 @@
+
+from spot_kinematics import SpotKinematics, LegKinematics
+
+from math import pi
+
+smk = SpotKinematics()
+smk.set_body_angles(body_pitch_rad = 0.75, body_roll_rad = 0.0, body_yaw_rad = 0.0)
